40c34e04ff6840679b0a6c73f291254ec251e3a3
gabrielcaiana <gabrielcaiana@hotmail.com>
2020-09-22 00:46:37 -0300

    add: Operadores de Incremento e Decremento

---
+++ JavaProjects/src/com/java/OperadoresIncrementoDecremento.java
@@ -0,0 +1,13 @@
+package com.java;
+
+public class OperadoresIncrementoDecremento {
+
+	public static void main(String[] args) {
+		int numero = 10;
+		numero++;
+		System.out.println(numero);
+		numero--;
+		System.out.println(numero);
+	}
+
+}
