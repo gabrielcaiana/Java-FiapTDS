3c4c9e6fd065d66de8e94cc6d6708c11b7b9e788
gabrielcaiana <gabrielcaiana@hotmail.com>
2020-09-22 00:14:49 -0300

    Create repository

---
+++ JavaProjects/src/com/java/ConversaoTiposPrimitivos.java
@@ -0,0 +1,15 @@
+package com.java;
+
+public class ConversaoTiposPrimitivos {
+
+	public static void main(String[] args) {
+		int x = 10; //declaração de uma variável do tipo int
+		double d = x; // variável double recebe um tipo int
+		long l = x; // variável long recebe um tipo int
+		float f = x; // variável float recebe um tipo int
+		short s = 20; //declaração de uma variável do tipo short
+		x = s; // variável int recebe um tipo short
+		
+		System.out.println(x);
+	}
+}
---
+++ JavaProjects/src/com/java/PraticaTipoPrimitivos.java
@@ -0,0 +1,22 @@
+package com.java;
+
+public class PraticaTipoPrimitivos {
+
+	public static void main(String[] args) {
+		long codigo = 1523156;
+		int batimento = 65;
+		float peso = 85.5f;
+		double altura = 1.8;
+		boolean fumante = false;
+		char sexo = 'M';
+		
+		System.out.println(codigo);
+		System.out.println(batimento);
+		System.out.println(peso);
+		System.out.println(altura);
+		System.out.println(fumante);
+		System.out.println(sexo);
+
+	}
+
+}
